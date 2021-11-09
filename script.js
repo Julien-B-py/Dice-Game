85e6c01dc6cf03a2c7cbd5753923b9ca392de65e
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-11-09 14:11:42 +0100

    v0.2
    Add comments to JS code.
    Add user feedback to show winner.
    Edit and clean CSS code.

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+// Generate two random numbers between 1 and 6 and store in variables
 var diceN1 = Math.floor(Math.random() * 6) + 1;
 var diceN2 = Math.floor(Math.random() * 6) + 1;
 
-var imgP1 = document.querySelector(".img1");
-imgP1.src = "images/Dice-" + diceN1 + ".png";
+// Create a variable to store the winner
+var winner;
+// Compare dice values and adjust winner value depending on the result
+if (diceN1 > diceN2) {
+  winner = "Player 1 wins!";
+} else if (diceN2 > diceN1) {
+  winner = "Player 2 wins!";
+} else {
+  winner = "Draw!";
+}
 
+// Store requested css selectors in variables
+var imgP1 = document.querySelector(".img1");
 var imgP2 = document.querySelector(".img2");
+
+// Set the src attribute to display the corresponding image based on dice value
+imgP1.src = "images/Dice-" + diceN1 + ".png";
 imgP2.src = "images/Dice-" + diceN2 + ".png";
 
-if (diceN1 > diceN2) {
-  console.log("Player 1 wins");
-} else if (diceN2 > diceN1) {
-  console.log("Player 2 wins");
-} else {
-  console.log("Draw");
-}
+/* Get h2 element then set the textContent property to be equal to the string 
+defined above */
+var winnerTxt = document.querySelector("h2");
+winnerTxt.textContent = winner;
